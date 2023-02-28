78b4e56d7b5d3c5c4fb43d087b84c50edcdf86c0
Michael Woo <michaelwoo36@gmail.com>
2023-02-27 19:48:45 -0800

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,52 @@
+// Michael Woo
+// Lab 5: Towers of Hanoi
+// CS 145
+// Eclipse
+// This program solves the towers of Hanoi. The rules of the program are that no move can be rewinded, disk moved back to where it came from, 
+// and no moves from tower 1 to tower 3. 
 
 public class Main {
-	// no a - c or c -a
-	// No repeating previous move
 	
 	public static void main(String[]args) {
+
 		Tower tower = new Tower();
+		
 		System.out.println(solve(tower, 1));
 	}
 	
+	// Recursive Method
 	private static boolean solve(Tower tower, int stack) {
 		tower.print();
+		
 		if(tower.check()) {
+		
 			return true;
 		}
+		
 		else {
+			
+			// Checks for available tower
 			while(tower.available()) {
+				
+				// Checks if moving to the tower is legal
 				if(tower.safe(stack)) {
+					
+					// places disk onto tower
 					tower.place(stack);
+					
 					if(solve(tower, stack)) {
+					
 						return true;
 					}
-					
 				}
+				
+				// switches through different towers.
 				stack++;
+				
 				if(stack == 4) {
+				
 					stack = 1;
 				}
-			
-			
 			}
 			
 			return false;
--- src/Tower.java
+++ src/Tower.java
@@ -2,147 +2,193 @@ import java.util.Stack;
 
 public class Tower {
 	
-	public Stack<Integer> tower1= new Stack<>();
-	public Stack<Integer> tower2= new Stack<>();
-	public Stack<Integer> tower3= new Stack<>();
-	int to;
-	int from;
+	private Stack <Integer> tower1= new Stack <>();
+	private Stack <Integer> tower2= new Stack <>();
+	private Stack <Integer> tower3= new Stack <>();
+	
+	// Where disk is moved to.
+	private int to;
+	// Where disk is moved from.
+	private int from;
 	
 	public Tower() {
 		
+		// Places disks onto tower.
 		for(int i = 4; i >= 1; i--) {
+			
 			tower1.push(i);
 		}
 	}
 	
+	// If tower 3 is filled.
 	public boolean check() {
 		
 		if(tower3.size() == 4 ) {
+			
 			return true;
 		}
 		else {
+			
 			return false;
 		}
 		
 	}
 	
+	// Checks whether or not it is ok to move the top disk from the tower.
 	public boolean safe(int stack) {
 		
 		if(stack == 1 && !(tower1.isEmpty()) && to != 1 && (tower2.isEmpty() || tower1.peek() < tower2.peek())) {
+			
 			return true;
 			
 		}
+		
 		else if(stack == 2 && !(tower2.isEmpty())) {
+		
 			if(from == 1 && (tower3.isEmpty() || tower2.peek() < tower3.peek())) {
+			
 				return true;
 			}
 			
 			else if(from == 3 && (tower1.isEmpty() || tower2.peek() < tower1.peek())) {
+			
 				return true;
 			}
+			
 			else if(from == 2 && (tower3.isEmpty() || tower1.isEmpty() || tower2.peek() < tower3.peek() || tower2.peek() < tower1.peek())) {
+			
 				return true;
 			}
+			
 			else {
+			
 				return false;
 			}
 		}
 		
 		else if(stack == 3 && !(tower3.isEmpty()) && to != 3 && (tower2.isEmpty() || tower3.peek() < tower2.peek())) {
+			
 			return true;
 		}
 		
 		else {
+			
 			return false;
 		}
-		
 	}
 	
+	
+	// Moves disk from one tower to another.
 	public void place(int stack) {
 		
 		if(stack == 1) {
+	
 			tower2.push(tower1.pop());
+			
+			// updating moves.
 			to = 2;
 			from = 1;
 		}
 		
 		if (stack == 2) {
+			
 			if(from == 1) {
+			
 				tower3.push(tower2.pop());
+				
 				to = 3;
 				from = 2;
 			}
+			
 			else if(from == 3) {
+			
 				tower1.push(tower2.pop());
+				
 				to = 1;
 				from = 2;
 			}
+			
 			else if(from == 2) {
+			
 				if(tower1.isEmpty() || tower2.peek() < tower1.peek()) {
+				
 					tower1.push(tower2.pop());
+					
 					to = 1;
 					from = 2;
 				}
+				
 				if(tower3.isEmpty() || tower2.peek() < tower3.peek()) {
+				
 					tower3.push(tower2.pop());
+					
 					to = 3;
 					from = 2;
 				}
 			}
-		
 		}
+		
 		if(stack == 3) {
+		
 			tower2.push(tower3.pop());
+			
 			to = 2;
 			from = 3;
 		}
-		
 	}
 	
+	// Prints out towers and their disks
 	public void print() {
+		
 		System.out.println(tower1);
 		System.out.println(tower2);
 		System.out.println(tower3);
 		System.out.println();
 	}
 	
+	// Checks if any of the towers are able to be moved to and from.
 	public boolean available() {
 		
 		if((!(tower3.isEmpty()) && to != 3) && (tower2.isEmpty() || tower3.peek() < tower2.peek())) {
+			
 			return true;
 		}
 	
 		if((!(tower1.isEmpty()) && to != 1) && (tower2.isEmpty() || tower1.peek() < tower2.peek())) {
+			
 			return true;
 		}
 		
 			
 		if(!(tower2.isEmpty())) {
+			
 			if((from == 1) && (tower3.isEmpty() || tower2.peek() < tower3.peek())) {
+			
 				return true;
 			}
 			
+			
 			else if((from == 3) && (tower1.isEmpty() || tower2.peek() < tower1.peek())) {
+			
 				return true;
 			}
+			
 			else if(from == 2 && (tower3.isEmpty() || tower1.isEmpty() || tower2.peek() < tower3.peek() || tower2.peek() < tower1.peek())) {
+			
 				return true;
 			}
+			
 			else {
+				
 				return false;
 			}
 		}
 		
-		 
-		
 		else {
+			
 			return false;
-		
-		
 		}
-		
 	}
-	
 }
 
 
